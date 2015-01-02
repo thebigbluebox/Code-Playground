1ed9cc806651ebb55c71ab45d781a22949e87cb1
thebigbluebox <tong_901@hotmail.com>
2015-01-01 21:29:31 -0500

    Deleted files here, moved project to new repo

--- Terrain Generator/Assignment 3/Header.h
+++
@@ -1,3 +0,0 @@
-//#include <windows.h>
-#include <GL/glut.h>
-#include <GL/gl.h>
--- Terrain Generator/Assignment 3/boilerplateClass.cpp
+++
@@ -1,7 +0,0 @@
-#include "boilerplateClass.h"
-#include <stdio.h> //needed for printf command
-
-Boilerplate::Boilerplate(){	//constructor
-	publicVar = 0;
-	privateVar = 1;
-}
