5229dd11af741dba164a920862b238e4b77c1ac9
thebigbluebox <me@tairuitong.me>
2018-03-14 23:26:00 -0400

    Adding 9 but incomplete need to see if my author changes are correct

---
+++ Algorithms/Java/src/CTCI/CH1/q9.java
@@ -0,0 +1,13 @@
+package CTCI.CH1;
+
+public class q9 {
+
+    public static boolean checkStringRotation(String a, String b){
+        for(int index = 0; index < a.length(); index++){
+            for(int indey = 0; indey < a.length(); indey++){
+                if(a.charAt(indey) )
+            }
+        }
+        return false;
+    }
+}
