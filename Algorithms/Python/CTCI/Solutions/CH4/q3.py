f1613fdbfcf628d42678d691aefd623cc68c1304
thebigbluebox <me@tairuitong.me>
2018-04-21 22:40:01 -0400

    Adding more Chapter 4 questions

--- Algorithms/Python/CTCI/Solutions/CH4/q3.py
+++ Algorithms/Python/CTCI/Solutions/CH4/q3.py
@@ -1,18 +1,18 @@
-import queue
-from DataStructures.BinaryTree import *
-
-def listOfDepth(graph, item):
+def listOfDepth(graph):
     a = dict()
-    return __dfsListOfDepth(graph.getRoot(), 1, a)
+    __dfsListOfDepth(graph.getRoot(), 1, a)
+    return a;
 
 def __dfsListOfDepth(node, level, map):
     if level in map:
-        map[level].append(node.get_value())
+        if node is not None:
+            map[level].append(node.get_value())
     else:
         newList = []
-        newList.append(node.get_value())
+        if node is not None:
+            newList.append(node.get_value())
         map[level] = newList
     if node == None:
-        return
-    __dfsListOfDepth(node.get_left(),level+1)
-    __dfsListOfDepth(node.get_right(), level+1)
+        return map
+    __dfsListOfDepth(node.get_left(), level+1, map)
+    __dfsListOfDepth(node.get_right(), level+1, map)
--- Algorithms/Python/CTCI/Solutions/CH4/q8.py
+++ Algorithms/Python/CTCI/Solutions/CH4/q8.py
@@ -1,3 +1,47 @@
 #First Common Ancestor: Design an algorithm and write code to find the first common ancestor
 # of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
 # necessarily a binary search tree.
+from DataStructures.BinaryTree import *
+
+def commonAncestor(tree, node_a, node_b):
+    actual_node_a = postOrderTraversal(tree.getRoot(), node_a)
+    actual_node_b = postOrderTraversal(tree.getRoot(), node_b)
+
+    if postOrderTraversal(actual_node_a, node_b) is not None:
+        return node_a
+    if postOrderTraversal(actual_node_b, node_a) is not None:
+        return node_b
+
+    current_node = actual_node_a
+    while current_node is not None:
+        current_node = current_node.get_parent()
+        result = postOrderTraversal(current_node, node_b)
+        if result is not None:
+            return current_node.get_value()
+
+
+
+def postOrderTraversal(rootNode, value):
+    if(rootNode != None):
+        if rootNode.get_value() == value:
+            return rootNode
+        a = postOrderTraversal(rootNode.get_left(), value)
+        b = postOrderTraversal(rootNode.get_right(), value)
+        if a != None:
+            return a
+        if b != None:
+            return b
+
+
+testTree = Tree()
+testTree.add(20)
+testTree.add(30)
+testTree.add(10)
+testTree.add(15)
+testTree.add(5)
+testTree.add(17)
+testTree.add(3)
+testTree.add(7)
+
+value = commonAncestor(testTree, 7, 17)
+print(value)
